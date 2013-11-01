f0c42c81dc3c2e3739d0d2af81507463854273af
Andy Cross <andy@elastacloud.com>
2013-11-01 11:18:52 +0000

    Add support for Queues

---
+++ Elastacloud.AzureBlobDemo/Queue/QueueMessageWrapper.cs
@@ -0,0 +1,9 @@
+namespace Elastacloud.AzureBlobDemo.Queue
+{
+    public class QueueMessageWrapper
+    {
+        public string Message { get; set; }
+        public string PopReceipt { get; set; }
+        public string MessageId { get; set; }
+    }
+}
