d960917a7c5a333fa634e9afcd16135efe216db8
andy@elastacloud.com <andy@elastacloud.com>
2013-11-03 15:47:07 +0000

    rename folder

--- Elastacloud.AzureBlobDemo/Queue/QueueMessageWrapper.cs
+++
@@ -1,9 +0,0 @@
-namespace netmfazurestorage.Queue
-{
-    public class QueueMessageWrapper
-    {
-        public string Message { get; set; }
-        public string PopReceipt { get; set; }
-        public string MessageId { get; set; }
-    }
-}
