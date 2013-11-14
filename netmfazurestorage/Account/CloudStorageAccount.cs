4c73e5917d9693a155d890c0973d779f5fe6af02
Andy Cross <andy@elastacloud.com>
2013-11-14 08:19:38 +0000

    add in CloudStorageAccount holder

---
+++ netmfazurestorage/Account/CloudStorageAccount.cs
@@ -0,0 +1,23 @@
+﻿using NetMf.CommonExtensions;
+
+namespace netmfazurestorage.Account
+{
+    public class CloudStorageAccount
+    {
+        public string AccountName { get; set; }
+        public string AccountKey { get; set; }
+        public string UriEndpoints { get; set; }
+
+        public CloudStorageAccount(string accountName, string accountKey, string uriEndpoints)
+        {
+            AccountName = accountName;
+            AccountKey = accountKey;
+            UriEndpoints = uriEndpoints;
+        }
+
+        public CloudStorageAccount(string accountName, string accountKey) : this (accountName,accountKey,StringUtility.Format("http://{0}.blob.core.windows.net/", accountName))
+        {
+            
+        }
+    }
+}
